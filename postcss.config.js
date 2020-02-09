a0773a47c06522c81b74c5b29b78ac7c00bc89ac
Claire Squire <squirec@tjmac350.traveljigsaw.local>
2020-02-09 13:55:39 +0000

    postcss functions

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,13 @@ module.exports = {
         require('precss'),
         require('autoprefixer'),
         require('cssnano'),
+        require('postcss-for'),
+        require('postcss-functions')({
+            functions: {
+                percentage: function (numerator, denominator) {
+                    return `${(numerator / denominator) * 100}%`;
+                }
+            }
+        }),
     ]
 } 
