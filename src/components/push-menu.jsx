e1e3a690e90b961b557d6a937b80486acd86d749
Claire Squire <squirec@tjmac350.traveljigsaw.local>
2020-02-25 08:04:40 +0000

    close menu on link click

--- src/components/push-menu.jsx
+++ src/components/push-menu.jsx
@@ -27,22 +27,42 @@ const PushMenu = () => {
                     <div className="fd-menu__logo" data-test="fd-menu__logo" aria-hidden={true}></div>
                     <ul className="fd-menu__link-wrapper">
                         <li>
-                            <a href="#homePage" className="fd-menu__link" data-test="fd-menu__link">
+                            <a
+                                href="#homePage"
+                                onClick={() => setToggle(!toggle)}
+                                className="fd-menu__link"
+                                data-test="fd-menu__link"
+                            >
                                 Home
                             </a>
                         </li>
                         <li>
-                            <a href="#aboutPage" className="fd-menu__link" data-test="fd-menu__link">
+                            <a
+                                href="#aboutPage"
+                                onClick={() => setToggle(!toggle)}
+                                className="fd-menu__link"
+                                data-test="fd-menu__link"
+                            >
                                 About
                             </a>
                         </li>
                         <li>
-                            <a href="#workPage" className="fd-menu__link" data-test="fd-menu__link">
+                            <a
+                                href="#workPage"
+                                onClick={() => setToggle(!toggle)}
+                                className="fd-menu__link"
+                                data-test="fd-menu__link"
+                            >
                                 Work
                             </a>
                         </li>
                         <li>
-                            <a href="#contactPage" className="fd-menu__link" data-test="fd-menu__link">
+                            <a
+                                href="#contactPage"
+                                onClick={() => setToggle(!toggle)}
+                                className="fd-menu__link"
+                                data-test="fd-menu__link"
+                            >
                                 Contact
                             </a>
                         </li>
