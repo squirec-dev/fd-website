95db48f3452c2fd8d771f7f6a33f0dec7ecf88fb
unknown <squirec.dev@gmail.com>
2021-03-26 11:32:59 +0000

    refactor: extract footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = (): React.ReactElement => (
+    <footer className='c-footer'>
+        <div className='u-container u-p+'>
+            <div className='c-footer__icon' />
+            Designed &amp; Built by Claire Squire
+            {/* TODO:
+            <br />
+            * #GithubStars | |/ #GithubShares
+            */}
+        </div>
+    </footer>
+);
+
+export default Footer;
