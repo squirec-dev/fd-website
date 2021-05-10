fb8c1ac79b0ccee8c9aee5778c629f70dc2b215c
unknown <squirec.dev@gmail.com>
2021-05-10 21:08:50 +0100

    feat: v1 header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,10 +5,8 @@ interface HeaderProps {
 const Header = ({
     children,
 }: HeaderProps): React.ReactElement => (
-    <header className='c-header'>    
-        <div className='u-container'>
-            { children }
-        </div>
+    <header className='c-header'> 
+        { children }
     </header>
 );
 
