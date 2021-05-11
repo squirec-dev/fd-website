9597721e02dcda1dd91fe659cfbbace8fe41e7c4
unknown <squirec.dev@gmail.com>
2021-05-11 17:27:52 +0100

    refactor: about uses usp

--- src/modules/About/About.tsx
+++ src/modules/About/About.tsx
@@ -1,45 +1,46 @@
-import { ReactElement } from 'react';
-import {Card, CardHeader, CardBody} from '../../components/Card';
+import SVG from '../../components/SVGImage';
+import {
+    Css,
+    Html,
+    Javascript,
+    Node,
+    Reactjs,
+    Typescript
+} from '../../objects/brands';
+import USP from '../../components/USP';
 
-const About = (): ReactElement => (
-    <section id="aboutSection">
-		<div className="container">
-            <h2>About</h2>
-            <div className="fd-grid__row">
-                <div className="fd-grid__col3">
-                    <Card>
-                        <CardHeader backgroundClass="droplet" header="+XP" />
-                        <CardBody>Over 6 years commercial experience</CardBody>
-                    </Card>
+const About = (): React.ReactElement => (
+    <section id="aboutSection" className="u-background-light u-border-angled-primary">
+        <div className='u-container u-pt+'>
+            <h2>About me</h2>
+
+            <USP />
+
+            <p>Hello! I'm Claire, a front-end software engineer based near Manchester, UK</p>
+            <p>I'm passionate about creating amazing user experiences - making fun, easy to use and accessible websites</p>
+            <p>I like building reusable design systems, learning and sharing new things</p>
+            <p>When I'm not coding I like to get creative using a range of art mediums and get outdoors, either off on mini adventures or trying to grow things in my garden</p>
+        
+            <div className='u-mt+ u-mb+ c-masonry__container'>
+                <div className='c-masonry__block u-brand-js'>
+                    <SVG id="AboutPageJs" {...Javascript} />
                 </div>
-                <div className="fd-grid__col3">
-                    <Card>
-                        <CardHeader backgroundClass="droplet" header="UX" />
-                        <CardBody>Creating beautiful user experiences</CardBody>
-                    </Card>
+                <div className='c-masonry__block u-brand-ts'>
+                    <SVG id="AboutPageTs" {...Typescript} />
                 </div>
-                <div className="fd-grid__col3">
-                    <Card>
-                        <CardHeader backgroundClass="droplet" header="AA +" />
-                        <CardBody>Making inclusive &amp; accessible web content</CardBody>
-                    </Card>
+                <div className='c-masonry__block u-brand-css'>
+                    <SVG id="AboutPageCss" {...Css} />
+                </div>
+                <div className='c-masonry__block u-brand-react'>
+                    <SVG id="AboutPagereact" {...Reactjs} />
+                </div>
+                <div className='c-masonry__block u-brand-html'>
+                    <SVG id="AboutPageHtml" {...Html} />
+                </div>
+                <div className='c-masonry__block u-brand-node'>
+                    <SVG id="AboutPageNode" {...Node} />
                 </div>
             </div>
-            
-            {/* <img src="/images/droplet.png" /> */}
-            <p>Hi, I'm Claire.</p>
-            <p>I'm a frontend software engineer passionate about building design systems and providing amazing user experiences.</p>
-            <p>I am experienced in using:</p>
-            <ul>
-                <li>ReactJS &amp; NextJS frameworks</li>
-                <li>JavaScript, TypeScript &amp; jQuery</li>
-                <li>Semantic HTML &amp; micro-frontends</li>
-                <li>CSS, PostCSS &amp; preprocessors i.e. LESS, SASS</li>
-                <li>Version control i.e. Git, SVN</li>
-                <li>Test frameworks i.e. Jest, Cypress, Cucumber, Nightwatch, Selenium</li>
-                <li>NodeJS environments with Docker &amp; CI/CD deployment tools i.e. AWS, Bamboo</li>
-                <li>Build tools &amp; compilers i.e. Webpack, Grunt, Gulp, Babel</li>
-            </ul>
         </div>
     </section>
 );
