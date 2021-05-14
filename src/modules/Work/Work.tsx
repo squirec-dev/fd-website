04fd6d8957c7685ada1501f0f320f6607f5d51ec
unknown <squirec.dev@gmail.com>
2021-05-14 14:05:51 +0100

    refactor: work uses accordion component

--- src/modules/Work/Work.tsx
+++ src/modules/Work/Work.tsx
@@ -1,13 +1,27 @@
-import TabPanel from "../../components/TabPanel";
+import React from "react";
+import Accordion from "../../components/Accordion";
 import Jobs from "./Jobs";
 
-const Work = (): React.ReactElement => (
-    <section id="workSection" className="u-color-light u-background-dark u-border-angled-primary">
-        <div className="u-container">
-            <h2>My work</h2>
-            <TabPanel content={Jobs} />
-        </div>
-    </section>
-);
+const Work = (): React.ReactElement => {
+    const getAccordion = (job: string): React.ReactElement => {
+        const Component = Jobs[job];
+        return (
+            <Component />
+        );
+    };
+
+    return (
+        <section id="workSection" className="u-color-light u-background-dark u-border-angled-primary">
+            <div className="u-container">
+                <h2>My work</h2>
+                {Object.keys(Jobs).map((job, idx) => (
+                    <Accordion id={job} key={idx} title={job}>
+                        {getAccordion(job)}
+                    </Accordion>
+                ))}
+            </div>
+        </section>
+    );
+};
 
 export default Work;
