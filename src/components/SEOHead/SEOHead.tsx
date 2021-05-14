df42fdf6648f5d70e0fc15f9bdd9e3e0abe4266d
unknown <squirec.dev@gmail.com>
2021-05-14 14:18:29 +0100

    refactor: head manifest and favicon

--- src/components/SEOHead/SEOHead.tsx
+++ src/components/SEOHead/SEOHead.tsx
@@ -8,15 +8,11 @@ const SEOHead = (): React.ReactElement => (
         <meta name="keywords" content="aws, css, es6, frontend, html, javascript, js, next, node, postcss, react, sass, software engineer" />
         <meta name="robots" content="index, follow" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {/* TODO: <link rel="canonical" href="http://localhost:3000/" /> */}
-        {/* Favicon */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/images/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="/images/favicon/safari-pinned-tab.svg" color="#63c1e0" />
-        <meta name="msapplication-TileColor" content="#63c1e0" />
-        <meta name="theme-color" content="#ffffff" />
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
 
         <title>Fluid Designs | Claire Squire</title>
     </Head>
